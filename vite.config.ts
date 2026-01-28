0c374617f1b4f41c952ff22be41279d2b9e3fbdd
mlopezCorstar <mlopez@corstar.com>
2026-01-28 11:34:46 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/CorstarWebsite/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
